b87d954ecaba3cb42626efcf96da8396244d503b
Hemant Rajput <hrajput1272004@gmail.com>
2026-01-28 08:36:55 +0530

    feat: integrate Cloudinary for user profile photo management and update user photo handling

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,8 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import cloudinary from "./src/config/cloudinary.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
@@ -11,14 +10,14 @@ import UserRouter from "./src/routers/userRouter.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173",credentials:true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
-app.use("/public",PublicRouter);
-app.use("/user",UserRouter);
+app.use("/public", PublicRouter);
+app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is Working");
@@ -27,12 +26,20 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   const ErrorMessage = err.message || "Internal Server Error";
   const StatusCode = err.statusCode || 500;
+  console.log("Error Found ", { ErrorMessage, StatusCode });
 
   res.status(StatusCode).json({ message: ErrorMessage });
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+
+app.listen(port, async () => {
   console.log("Server Started at Port: ", port);
   connectDB();
+  try {
+    const res  = await cloudinary.api.ping();
+    console.log("Clodinary API is Working :",res);
+  } catch (error) {
+    console.error("Error Connecting Clodinary API :",error)
+  }
 });

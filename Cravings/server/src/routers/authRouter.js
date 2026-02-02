7270919ed79799ad48fb64b929bbdef28d0c93a3
Hemant Rajput <hrajput1272004@gmail.com>
2026-02-02 18:41:14 +0530

    feat: implement password reset functionality with OTP verification and email notifications

--- Cravings/server/src/routers/authRouter.js
+++ Cravings/server/src/routers/authRouter.js
@@ -1,10 +1,22 @@
-import express from 'express'
-import { UserLogin,UserRegister,UserLogout } from '../controllers/authControllers.js';
+import express from "express";
+import {
+  UserRegister,
+  UserLogin,
+  UserLogout,
+  UserGenOTP,
+  UserVerifyOtp,
+  UserForgetPassword
+} from "../controllers/authControllers.js";
+import { OtpProtect } from "../middlewares/authMiddlewares.js";
+
 const router = express.Router();
 
+router.post("/register", UserRegister);
+router.post("/login", UserLogin);
+router.get("/logout", UserLogout);
 
-router.post("/register",UserRegister);
-router.post("/login",UserLogin);
-router.get("/logout",UserLogout);
+router.post("/genOtp", UserGenOTP);
+router.post("/verifyOtp", UserVerifyOtp);
+router.post("/forgetPasword",OtpProtect,UserForgetPassword)
 
 export default router;
--- Cravings/server/src/utils/authToken.js
+++ Cravings/server/src/utils/authToken.js
@@ -4,7 +4,7 @@ export const genToken = (user, res) => {
   try {
     const payload = {
       id: user._id,
-      role: user.role || "admin",
+      role: user.role,
     };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "1d",
@@ -14,6 +14,28 @@ export const genToken = (user, res) => {
 
     res.cookie("parleG", token, {
       maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+export const genOtpToken = (user, res) => {
+  try {
+    const payload = {
+      id: user._id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "10m",
+    });
+
+    console.log(token);
+
+    res.cookie("otpToken", token, {
+      maxAge: 1000 * 60 * 10,
       httpOnly: true,
       secure: false,
       sameSite: "lax",
